3b82745dddf2b86e1406a5a91c6fc2390e83c7f7
LongVu2190 <hoanglongvu912@gmail.com>
2024-04-25 21:48:33 +0700

    upload multiple files

--- app/src/main/java/com/example/mobile_midterm/fragments/UploadFragment.java
+++ app/src/main/java/com/example/mobile_midterm/fragments/UploadFragment.java
@@ -3,11 +3,14 @@ package com.example.mobile_midterm.fragments;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +38,7 @@ import com.google.firebase.storage.UploadTask;
 import java.util.Objects;
 
 public class UploadFragment extends Fragment {
+
     private ImageView uploadImage;
     EditText uploadName;
     ProgressBar progressBar;
@@ -47,6 +51,7 @@ public class UploadFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_upload, container, false);
 
         FloatingActionButton uploadButton = view.findViewById(R.id.uploadButton);
+        FloatingActionButton uploadMultipleButton = view.findViewById(R.id.uploadMultipleButton);
         uploadName = view.findViewById(R.id.uploadName);
         uploadImage = view.findViewById(R.id.uploadImage);
         progressBar = view.findViewById(R.id.progressBar);
@@ -56,8 +61,21 @@ public class UploadFragment extends Fragment {
             if (result.getResultCode() == Activity.RESULT_OK) {
                 Intent data = result.getData();
                 assert data != null;
-                imageUri = data.getData();
-                uploadImage.setImageURI(imageUri);
+                if (data.getClipData() != null) { // Handling multiple images
+                    int count = data.getClipData().getItemCount();
+                    if (count > 1) {
+                        for (int i = 0; i < count; i++) {
+                            Uri imageUri = data.getClipData().getItemAt(i).getUri();
+                            uploadToFirebase(imageUri); // Call the upload function for each image
+                        }
+                    } else {
+                        imageUri = data.getClipData().getItemAt(0).getUri();
+                        uploadImage.setImageURI(imageUri);
+                    }
+                } else if (data.getData() != null) { // Handling single image selection
+                    imageUri = data.getData();
+                    uploadImage.setImageURI(imageUri);
+                }
             } else {
                 Toast.makeText(getContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
             }
@@ -78,15 +96,26 @@ public class UploadFragment extends Fragment {
             }
         });
 
+        uploadMultipleButton.setOnClickListener(view1 -> {
+            Intent photoPicker = new Intent();
+            photoPicker.setAction(Intent.ACTION_GET_CONTENT);
+            photoPicker.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            photoPicker.setType("image/*");
+            activityResultLauncher.launch(photoPicker);
+        });
+
         return view;
     }
 
     private void uploadToFirebase(Uri uri) {
         String upload_name = uploadName.getText().toString();
         if (upload_name.isEmpty()) {
-            upload_name = String.valueOf(System.currentTimeMillis());
+            upload_name = getFileName(uri);
         }
-        final StorageReference imageReference = storageReference.child(upload_name + "." + getFileExtension(uri));
+        else {
+            upload_name = upload_name + "." + getFileExtension(uri);
+        }
+        final StorageReference imageReference = storageReference.child(upload_name);
         imageReference.putFile(uri).addOnSuccessListener(taskSnapshot -> imageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri1) {
@@ -108,6 +137,29 @@ public class UploadFragment extends Fragment {
                 Toast.makeText(getContext(), "Failed", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @SuppressLint("Range")
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContext().getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 
     private String getFileExtension(Uri fileUri) {
